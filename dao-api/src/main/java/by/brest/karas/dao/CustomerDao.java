5c149a2f3413b910a7dd2e5ef1f97a315336b9ca
tikhtmp <57909756+tikhtmp@users.noreply.github.com>
2022-01-23 14:39:08 +0300

    add service layer

---
+++ dao-api/src/main/java/by/brest/karas/dao/CustomerDao.java
@@ -0,0 +1,23 @@
+package by.brest.karas.dao;
+
+import by.brest.karas.model.Customer;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CustomerDao {
+
+    List<Customer> findAll();
+
+    Customer findByLogin(String login);
+
+    Optional<Customer> findById(Integer customerId);
+
+    Integer create (Customer customer);
+
+    Integer update(Customer updatedCustomer);
+
+    Integer delete(Integer customerId);
+
+    List<Customer> selectCustomers(String filter);
+}
